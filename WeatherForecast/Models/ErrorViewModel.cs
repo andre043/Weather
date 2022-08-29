848f0867a57ceb06822c1c0d6130cfb5c0147f67
andre043 <andrevosloo043@gmail.com>
2022-08-29 22:20:16 +0200

    Removed HTML JQUERY Up Skill Project

--- WeatherForecast/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WeatherForecast.Models
-{
-  public class ErrorViewModel
-  {
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-  }
-}
